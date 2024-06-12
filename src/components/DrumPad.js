825179d27ed59574c62510ef9043c053fa32f403
Marius Povilaitis <marius.povilaitis@asolvi.com>
2024-06-12 11:58:52 +0100

    fixed

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,49 +1,54 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { playSound } from '../redux/actions';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { playSound } from "../redux/actions";
 
 const DrumPad = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handlePadClick = (sound, key) => {
-        const audio = document.getElementById(key);
-        console.log(`Audio Element: `, audio);
-        if (audio instanceof HTMLAudioElement) {
-            console.log(`Playing sound: ${sound}, Key: ${key}`);
-            audio.currentTime = 0;
-            audio.play().catch(error => console.error(`Error playing sound: ${error}`));
-            dispatch(playSound(sound));
-        } else {
-            console.error(`Audio element with id ${key} not found or play method is not a function`);
-        }
-    };
+  const handlePadClick = (sound, key) => {
+    const audio = document.getElementById(key);
+    console.log(`Audio Element: `, audio);
+    if (audio instanceof HTMLAudioElement) {
+      console.log(`Playing sound: ${sound}, Key: ${key}`);
+      audio.currentTime = 0;
+      audio
+        .play()
+        .catch((error) => console.error(`Error playing sound: ${error}`));
+      dispatch(playSound(sound));
+    } else {
+      console.error(
+        `Audio element with id ${key} not found or play method is not a function`
+      );
+    }
+  };
 
-    const pad = [
-        { key: 'Q', sound: `public/sounds/Cev_H2.mp3` },
-        { key: 'W', sound: 'public/sounds/Dsc_Oh.mp3' },
-        { key: 'E', sound: 'public/sounds/Heater-2.mp3' },
-        { key: 'A', sound: 'public/sounds/Heater-3.mp3' },
-        { key: 'S', sound: 'public/sounds/Heater-4_1.mp3' },
-        { key: 'D', sound: 'public/sounds/Heater-6.mp3' },
-        { key: 'Z', sound: 'public/sounds/Kick_n_Hat.mp3' },
-        { key: 'X', sound: 'public/sounds/RP4_KICK_1.mp3' },
-    ];
+  const pad = [
+    { key: "Q", sound: "/sounds/Cev_H2.mp3" },
+    { key: "W", sound: "/sounds/Dsc_Oh.mp3" },
+    { key: "E", sound: "/sounds/Heater-2.mp3" },
+    { key: "A", sound: "/sounds/Heater-3.mp3" },
+    { key: "S", sound: "/sounds/Heater-4_1.mp3" },
+    { key: "D", sound: "/sounds/Heater-6.mp3" },
+    { key: "Z", sound: "/sounds/Kick_n_Hat.mp3" },
+    { key: "X", sound: "/sounds/RP4_KICK_1.mp3" },
+  ];
 
-    return (
-        <div className="drum-pad">
-            {pad.map(pad => (
-                <button
-                    key={pad.key}
-                    id={pad.key}
-                    className="drum-pad"
-                    onClick={() => handlePadClick(pad.sound, pad.key)}
-                >
-                    {pad.key}
-                    <audio className="clip" id={pad.sound} src={`public/sounds`}></audio>
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div className="drum-pad">
+      {pad.map((pad) => (
+        <>
+          <button
+            key={pad.key}
+            className="drum-pad"
+            onClick={() => handlePadClick(pad.sound, pad.key)}
+          >
+            {pad.key}
+          </button>
+          <audio className="clip" id={pad.key} src={pad.sound}></audio>
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default DrumPad;
